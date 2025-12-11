5bee0de9a07ba406abe1828be11bab7cd1fa7cc4
Scott Greff <68620832+scottiegreff@users.noreply.github.com>
2025-12-11 10:11:41 -0800

    Share Mongo init across Vercel handlers

--- api/user.go
+++ api/user.go
@@ -1,50 +1,6 @@
 package handler
 
-import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"sync"
-	"time"
-
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"trivTunes-backend/handlers"
-)
-
-var (
-	client   *mongo.Client
-	initOnce sync.Once
-	initErr  error
-)
-
-func ensureMongo() error {
-	initOnce.Do(func() {
-		_ = godotenv.Load()
-		uri := os.Getenv("MONGODB_URI")
-		if uri == "" {
-			initErr = fmt.Errorf("MONGODB_URI is not set")
-			return
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-
-		client, initErr = mongo.Connect(ctx, options.Client().ApplyURI(uri))
-		if initErr != nil {
-			return
-		}
-		if err := client.Ping(ctx, readpref.Primary()); err != nil {
-			initErr = err
-			return
-		}
-		handlers.InitUserCollection(client)
-	})
-	return initErr
-}
+import "net/http"
 
 // Handler is the Vercel entrypoint for /api/user.
 func Handler(w http.ResponseWriter, r *http.Request) {
