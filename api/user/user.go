fe75e3b2d09e858f125ef76cd8850f5a4046706e
Scott Greff <68620832+scottiegreff@users.noreply.github.com>
2025-12-11 10:17:19 -0800

    Inline Mongo init per Vercel function to avoid internal import

--- api/user/user.go
+++ api/user/user.go
@@ -1,15 +1,54 @@
 package handler
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"trivTunes-backend/handlers"
-	"trivTunes-backend/internal/mongoinit"
 )
+
+var (
+	client   *mongo.Client
+	initOnce sync.Once
+	initErr  error
+)
+
+func ensureMongo() error {
+	initOnce.Do(func() {
+		_ = godotenv.Load()
+		uri := os.Getenv("MONGODB_URI")
+		if uri == "" {
+			initErr = fmt.Errorf("MONGODB_URI is not set")
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		client, initErr = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+		if initErr != nil {
+			return
+		}
+		if err := client.Ping(ctx, readpref.Primary()); err != nil {
+			initErr = err
+			return
+		}
+		handlers.InitUserCollection(client)
+	})
+	return initErr
+}
 
 // Handler is the Vercel entrypoint for /api/user.
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if err := mongoinit.EnsureMongo(); err != nil {
+	if err := ensureMongo(); err != nil {
 		http.Error(w, "Server initialization error", http.StatusInternalServerError)
 		return
 	}
